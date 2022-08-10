8889732d115af14d3de4359da40408c1b617e295
Tech2mars <109243436+Tech2Future@users.noreply.github.com>
2022-08-11 00:52:36 +0530

    Add files via upload

---
+++ diret input.py
@@ -0,0 +1,12 @@
+import pyautogui  #if not installed, install using - pip install pyautogui
+import time
+
+time.sleep(5)
+
+#Paste your code below to get typed in adapt sprint section automatically.
+pyautogui.typewrite('''
+paste your code here
+''')
+
+
+
